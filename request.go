198abe5012b7310dd584258357c90544fa3859ed
ssailly <steven.sailly@gmail.com>
2023-12-06 10:43:58 +0100

    Applied review's suggestions

--- request.go
+++ request.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"crypto/sha256"
 	"encoding/binary"
 	"log"
@@ -8,22 +8,22 @@ import(
 )
 
 type request struct {
-	typeRq     uint8
-	value			[]byte
+	typeRq uint8
+	value  []byte
 }
 
 func toBytes(rq *request) []byte {
 	// extensions in 'Hello' request
 	ext := uint32(0)
-	
+
 	idRq := make([]byte, 4)
 	_, err := rand.Read(idRq)
 	if err != nil {
 		log.Fatal("rand.Read:", err)
 	}
 
-	extRq := make([]byte, 0)
-	hashRq := make([]byte, 0)
+	var extRq []byte
+	var hashRq []byte
 	if rq.typeRq == 2 {
 		extRq = binary.BigEndian.AppendUint32(extRq, ext)
 	}
@@ -34,26 +34,17 @@ func toBytes(rq *request) []byte {
 		h.Write(rq.value)
 		hashRq = h.Sum(nil)
 	}
-	length := len(extRq) + len(rq.value) + len(hashRq) 
-	res := make([]byte, 0)
+	length := len(extRq) + len(rq.value) + len(hashRq)
+
+	var res []byte
 	lengthRq := make([]byte, 0)
 	lengthRq = binary.BigEndian.AppendUint16(lengthRq, uint16(length))
-	for i := range idRq {
-		res = append(res, idRq[i])
-	}
+	res = append(res, idRq...)
 	res = append(res, rq.typeRq)
-	for i := range lengthRq {
-		res = append(res, lengthRq[i])
-	}
-	for i := range extRq {
-		res = append(res, extRq[i])
-	}
+	res = append(res, lengthRq...)
+	res = append(res, extRq...)
 	// TODO: add extensions if needed
-	for i := range hashRq {
-		res = append(res, hashRq[i])
-	}
-	for i := range rq.value {
-		res = append(res, rq.value[i])
-	}
+	res = append(res, hashRq...)
+	res = append(res, rq.value...)
 	return res
 }
