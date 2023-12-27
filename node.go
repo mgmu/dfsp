34d43ccd83ddba3c6b8dd927bf11e58e00076f67
mgmu <guillermo.moron@laposte.net>
2023-12-27 09:23:31 +0100

    Fixed index out of range error in node.from()

--- node.go
+++ node.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "errors"
 	"crypto/sha256"
 	"fmt"
 	"log"
@@ -31,28 +30,58 @@ type node struct {
 // Generates the Merkle tree structure corresponding to the tree structure
 // denoted by name. If an error occurs, returns nil and the error.
 func from(name string) (*node, error) {
+	if debug {
+		fmt.Printf("\n\nnode.from()\n")
+	}
 	f, err := os.Open(name)
 	if err != nil {
 		return nil, err
+	}
+	if debug {
+		fmt.Println("Open file")
 	}
 	info, err := f.Stat()
 	if err != nil {
 		return nil, err
 	}
+	if debug {
+		fmt.Println("Stat file")
+	}
 	mode := info.Mode()
 	size := info.Size()
 	if mode.IsRegular() {
+		if debug {
+			fmt.Println("File is regular")
+		}
 		if size > 32 * 1024 {
+			if debug {
+				fmt.Println("File too big, log.Fatal() incoming")
+			}
+			log.Fatal("node.from(): File size not yet supported...")
+		} else if size > 1024 && size <= 32 * 1024 {
+			if debug {
+				fmt.Printf("Big file of less than %d bytes\n", 32*1024)
+			}
 			data, err := os.ReadFile(name)
 			if err != nil {
 				return nil, err
+			}
+			if debug {
+				fmt.Println("Read content of regular file")
 			}
 			nbChildren := int(size / 1024)
 			if size % 1024 != 0 {
 				nbChildren++
 			}
+			if debug {
+				fmt.Printf("Size of file %d, number of chunk children %d\n",
+					size, nbChildren)
+			}
 			childs := make([]*node, nbChildren)
 			for i := 0; i < nbChildren; i++ {
+				if debug {
+					fmt.Printf("Creating chunk childs [iter: %d]\n", i)
+				}
 				tmp := data[i * 1024: min((i + 1) * 1024, len(data))]
 				childs[i] = &node{
 					category: Chunk,
@@ -67,13 +96,20 @@ func from(name string) (*node, error) {
 				children: childs,
 				name: name,
 			}
+			if debug {
+				fmt.Println("Returning BigFile")
+			}
 			return new, nil
-		} else if size > 1024 {
-			log.Fatal("node.from(): File size not yet supported...")
 		} else {
+			if debug {
+				fmt.Println("Little file")
+			}
 			data, err := os.ReadFile(name)
 			if err != nil {
 				return nil, err
+			}
+			if debug {
+				fmt.Println("Read regular file content")
 			}
 			new := node{
 				category: Chunk,
@@ -81,29 +117,49 @@ func from(name string) (*node, error) {
 				name: name,
 				data:     data,
 			}
+			if debug {
+				fmt.Println("Returning Chunk")
+			}
 			return &new, nil
 		}
 	}
 	if mode.IsDir() {
+		if debug {
+			fmt.Println("File is directory")
+		}
 		entries, err := os.ReadDir(name)
 		if err != nil {
 			return nil, err
 		}
+		if debug {
+			fmt.Println("Read entries")
+		}
 		if len(entries) > 16 {
 			return nil, fmt.Errorf("node.from(): too many entries in dir")
 		}
+		if debug {
+			fmt.Println("Less than 16 entries")
+		}
 		var children []*node
-		for i, child := range entries {
-			children[i], err = from(name + child.Name())
+		for _, child := range entries {
+			if debug {
+				fmt.Printf("Rec call to [%s]\n", name + "/" + child.Name())
+				
+			}
+			nd, err := from(name + "/" + child.Name())
 			if err != nil {
 				return nil, err
 			}
+			children = append(children, nd)
 		}
 		new := &node{
 			category: Directory,
 			hash: hashFrom(children),
 			children: children,
 			name: name,
+		}
+		if debug {
+			fmt.Printf("Returning directory with name %s\n", new.name)
 		}
 		return new, nil
 	}
